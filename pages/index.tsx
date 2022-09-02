af6b5c2a329bca1482a7bc207783b57d200d9aff
Veysel <veyselkartalms@gmail.com>
2022-09-03 00:00:23 +0300

    added global.scss & bg.jpg

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,12 +15,14 @@ const Home = () => {
       </Head>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map((post, index) => <PostCard post={post} key={index} />)}
-      </div>
-      <div className="lg:col-span-4 col-span-1">
-        <div className="lg:sticky relative top-8">
-          <PostWidget />
-          <Categories />
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => <PostCard post={post} key={index} />)}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
     </div>
