eea575f63bed73fafc13d17d38036e9258ff7613
Veysel <veyselkartalms@gmail.com>
2022-09-03 16:34:49 +0300

    layout added

--- components/Header.jsx
+++ components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
                     {categories.map((category) => (
                         <Link key={category.slug} href={`/category/${category.slug}`}>
                             <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-
+                                {category.name}
                             </span>
                         </Link>
                     ))}
