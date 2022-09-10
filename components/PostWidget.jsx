3410f32a2c50c1e740cc7d449ca62dc7427426d2
Veysel <veyselkartalms@gmail.com>
2022-09-11 00:54:39 +0300

    widget component added

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -17,8 +17,35 @@ function PostWidget({ categories, slug }) {
     }
   }, [slug])
 
+  console.log(relatedPosts);
+
   return (
-    <div>PostWidget</div>
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Post"}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img
+              src={post.featuredImage.url}
+              alt={post.title}
+              width="100px"
+              height="100px"
+              className="align-middle rounded-lg"
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </p>
+            <Link href={`/post/${post.slug}`} className="text-md" key={post.title}>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
