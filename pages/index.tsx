9bdfc57ffc04e0e26e382cef77c19209ed1add82
Veysel <veyselkartalms@gmail.com>
2022-09-10 23:16:20 +0300

    display posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home = ({ posts }: any) => {
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post: any, index: any) => <PostCard post={post} key={index} />)}
+          {posts.map((post: any, index: any) => <PostCard post={post.node} key={index} />)}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
