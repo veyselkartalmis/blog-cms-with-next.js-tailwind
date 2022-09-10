4cc75970a3253d43da6d4580b27add90e4ea260f
Veysel <veyselkartalms@gmail.com>
2022-09-10 22:13:24 +0300

    data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
 import Head from 'next/head';
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
-]
-
-const Home = () => {
+const Home = ({ posts }: any) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -16,7 +12,7 @@ const Home = () => {
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => <PostCard post={post} key={index} />)}
+          {posts.map((post: any, index: any) => <PostCard post={post} key={index} />)}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -29,4 +25,11 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []; //postları getir veya boş array dön.
+  return {
+    props: { posts }
+  }
+}
