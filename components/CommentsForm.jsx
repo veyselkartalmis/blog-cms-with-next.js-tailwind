7a4a8481ed75aa1fa77a0342cb0525d9bc9c41e1
Veysel <veyselkartalms@gmail.com>
2022-09-11 01:45:33 +0300

    components added

---
+++ components/CommentsForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CommentsForm() {
+  return (
+    <div>CommentsForm</div>
+  )
+}
+
+export default CommentsForm
---
+++ components/PostDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PostDetail() {
+  return (
+    <div>PostDetail</div>
+  )
+}
+
+export default PostDetail
