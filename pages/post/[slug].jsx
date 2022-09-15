969889b6d895ebe44814f689de36ea885a8e24f7
Veysel <veyselkartalms@gmail.com>
2022-09-15 21:22:50 +0300

    props added

--- pages/post/[slug].jsx
+++ pages/post/[slug].jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { getPosts, getPostDetails } from "../../services";
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from "../../components";
 
-function PostDetails() {
+function PostDetails({ post }) {
     return (
         <div className="container mx-auto px-10 mb-8">
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="col-span-1 lg:col-span-8">
-                    <PostDetail />
-                    <Author />
-                    <CommentsForm />
-                    <Comments />
+                    <PostDetail post={post} />
+                    <Author author={post.author} />
+                    <CommentsForm slug={post.slug} />
+                    <Comments slug={post.slug} />
                 </div>
                 <div className="col-span-1 lg:col-span-4">
                     <div className="relative lg:sticky top-8">
@@ -26,8 +26,8 @@ function PostDetails() {
 export default PostDetails;
 
 export async function getStaticProps({ params }) {
-    const data = (await getPostDetails(params.slug));
+    const data = await getPostDetails(params.slug);
     return {
-        props: { data }
+        props: { post: data }
     }
 }
