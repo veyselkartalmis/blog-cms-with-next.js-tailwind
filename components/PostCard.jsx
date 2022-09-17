a3e5763596a08c9fbeb1839976cb6c79eefca8a3
Veysel <veyselkartalms@gmail.com>
2022-09-17 20:22:04 +0300

    bug fixes :) some queries added

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -2,7 +2,6 @@ import moment from "moment";
 import Link from "next/link";
 
 function PostCard({ post }) {
-    console.log(post);
     return (
         <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
             <div className="relative overflow-hidden shadow-md pb-80 mb-6">
