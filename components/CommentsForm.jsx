7694b90f317f713686524c6df640c42e48c46e34
Veysel <veyselkartalms@gmail.com>
2022-09-17 23:50:31 +0300

    bug again :(

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { submitComment } from "../services";
 
 function CommentsForm({ slug }) {
   const [error, setError] = useState(false);
@@ -8,6 +9,11 @@ function CommentsForm({ slug }) {
   const nameEl = useRef();
   const emailEl = useRef();
   const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem("name");
+    emailEl.current.value = window.localStorage.getItem("email");
+  }, [])
 
   const handleCommentSubmission = () => {
     setError(false);
@@ -25,16 +31,24 @@ function CommentsForm({ slug }) {
     const commentObj = { name, email, comment, slug }
 
     if (storeData) {
-      localStorage.setItem("name", name);
-      localStorage.setItem("email", email);
+      window.localStorage.setItem("name", name);
+      window.localStorage.setItem("email", email);
     } else {
-      localStorage.removeItem("name", name);
-      localStorage.removeItem("email", email);
+      window.localStorage.removeItem("name", name);
+      window.localStorage.removeItem("email", email);
     }
+
+    submitComment(commentObj)
+      .then((res) => {
+        setShowSuccessMessage(true);
+        setTimeout(() => {
+          setShowSuccessMessage(false);
+        }, 3000);
+      })
   }
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comments Form</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a Reply</h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
           ref={commentEl}
