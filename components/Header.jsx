810d3155206d68a465ec3e7d412d5cc88a0d86b1
Veysel <veyselkartalms@gmail.com>
2022-10-07 17:20:17 +0300

    header update

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,19 +15,10 @@ function Header() {
             <div className="border-b w-full inline-block border-blue-400 py-8">
                 <div className="md:float-left block">
                     <Link href="/">
-                        <span className="cursur-pointer font-bold text-4xl text-white">
+                        <span className="cursor-pointer font-bold text-4xl text-white">
                             BlogCMS
                         </span>
                     </Link>
-                </div>
-                <div className="hidden md:float-left md:contents">
-                    {categories.map((category) => (
-                        <Link key={category.slug} href={`/category/${category.slug}`}>
-                            <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-                                {category.name}
-                            </span>
-                        </Link>
-                    ))}
                 </div>
             </div>
         </div>
